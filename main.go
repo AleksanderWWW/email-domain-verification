80b7ce2e16e75b5fd91e83ff25eed075dc002126
AleksanderWWW <alwojnarowicz@gmail.com>
2023-09-03 15:51:16 +0200

    get listenaddress from flags

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"log"
+	"flag"
 	"github.com/AleksanderWWW/email-domain-verification/api"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	listenAddr := flag.String("listenaddr", ":5555", "api port to listen on")
+	flag.Parse()
+
 	router := gin.Default()
 
 	router.POST("api/v1/verify", api.HandleVerification)
 
-	router.Run(":8080")
+	log.Fatal(router.Run(*listenAddr))
 }
